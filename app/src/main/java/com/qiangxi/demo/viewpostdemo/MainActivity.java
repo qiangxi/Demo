76d502c55878366e075386927a4c29e90653ffa6
Ray <renqiangqiang.china@gmail.com>
2019-01-09 16:19:55 +0800

    深入理解方法调用栈的作用

--- app/src/main/java/com/qiangxi/demo/viewpostdemo/MainActivity.java
+++ app/src/main/java/com/qiangxi/demo/viewpostdemo/MainActivity.java
@@ -1,14 +1,21 @@
 package com.qiangxi.demo.viewpostdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 
 import com.qiangxi.demo.R;
 
+import java.util.Random;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     private int count = 1000;
+    private Handler mHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,9 +24,42 @@ public class MainActivity extends AppCompatActivity {
         demo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                demo.setCount(count);
-                demo.start();
+                // demo.setCount(count);
+                // demo.start();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        test01("path1");
+                    }
+                }).start();
+                 new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        test01("path2");
+                    }
+                }).start();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        test01("path3");
+                    }
+                }).start();
             }
         });
+    }
+
+    /**
+     * 测试连续调用，参数不同，耗时不同的情况下，走到run方法时，参数是否还是正确的
+     */
+    private void test01(final String path) {
+        final long seconds = new Random().nextInt(4000) + 1000;
+        Log.e("tag", "path = " + path + ",seconds = " + seconds);
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.e("tag", "run path = " + path + ", seconds = " + seconds);
+            }
+        }, seconds);
+
     }
 }
